081b5228f2853c80d5b92cf70925858fb803923e
Ken <kennwheeler@hotmail.com>
2016-09-24 18:33:54 -0400

    Set Up
    Set up the package and the Main.java

---
+++ src/main/java/com/runemagic/forge/mods/Main.java
@@ -0,0 +1,18 @@
+package com.runemagic.forge.mods;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+
+public class Main {
+	public static final String MODID = "RuneMagic";
+	public static final String VERSION = "0.0.1";
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+}
