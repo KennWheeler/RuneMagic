a3c99070432fb74d98192a4c52c444004e9ea4d2
Ken <kennwheeler@hotmail.com>
2016-09-24 21:35:53 -0400

    Rune Essence Added
    Added Rune Essence Item that will come from minning Rune Essence Blocks.
    Rune Essence will be used to create the runes required for casting
    spells

--- src/main/java/com/runemagic/forge/mods/Main.java
+++ src/main/java/com/runemagic/forge/mods/Main.java
@@ -1,8 +1,10 @@
 package com.runemagic.forge.mods;
 
 import com.runemagic.forge.mods.ModBlocks.RuneEssenceBlock;
+import com.runemagic.forge.mods.ModItems.RuneEssence;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -14,11 +16,15 @@ public class Main {
 	public static final String MODID = "RuneMagic";
 	public static final String VERSION = "0.0.1";
 	public static Block runeEssenceBlock;
+	public static Item runeEssence;
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
 		runeEssenceBlock = new RuneEssenceBlock();
 		GameRegistry.registerBlock(runeEssenceBlock, "runeEssenceBlock");
+		
+		runeEssence = new RuneEssence();
+		GameRegistry.registerItem(runeEssence, "runeEssence");
 	}
 }
---
+++ src/main/java/com/runemagic/forge/mods/ModItems/RuneEssence.java
@@ -0,0 +1,13 @@
+package com.runemagic.forge.mods.ModItems;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class RuneEssence extends Item {
+	public RuneEssence(){
+		super();
+		this.setUnlocalizedName("runeEssence");
+		this.setCreativeTab(CreativeTabs.MATERIALS);
+	}
+
+}
