48f19217716e90862baf09e0e60f02880e2a6450
Ken <kennwheeler@hotmail.com>
2016-09-24 21:56:40 -0400

    Blocks drop essence
    Rune Essence blocks now drop between 3 and 5 rune essence when broken

--- src/main/java/com/runemagic/forge/mods/ModBlocks/RuneEssenceBlock.java
+++ src/main/java/com/runemagic/forge/mods/ModBlocks/RuneEssenceBlock.java
@@ -1,8 +1,14 @@
 package com.runemagic.forge.mods.ModBlocks;
 
+import java.util.Random;
+
+import com.runemagic.forge.mods.Main;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
 public class RuneEssenceBlock extends Block {
 	public RuneEssenceBlock() {
@@ -13,5 +19,12 @@ public class RuneEssenceBlock extends Block {
 		this.setHardness(2.0F);
 		this.setLightLevel(0.2F);
 	}
-
+	
+	public Item getItemDropped(IBlockState state, Random random, int i2){
+		return Main.runeEssence;
+	}
+	
+	public int quantityDropped(Random random){
+		return random.nextInt(2)+3;
+	}
 }
--- src/main/java/com/runemagic/forge/mods/WorldGen.java
+++ src/main/java/com/runemagic/forge/mods/WorldGen.java
@@ -9,9 +9,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
-import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
-import com.runemagic.forge.mods.ModBlocks.RuneEssenceBlock;
 
 public class WorldGen implements IWorldGenerator{
 	@Override
@@ -37,7 +35,7 @@ public class WorldGen implements IWorldGenerator{
 	}
 	
 	private void generateSurface(World world,Random random, int x, int z){
-		this.addBlockSpawn(ModBlocks.RuneEssenceBlock, world, random, x, z, 16, 16, 12, 1, 0, 32 );
+		this.addBlockSpawn(Main.runeEssenceBlock, world, random, x, z, 16, 16, 12, 1, 0, 32 );
 	}
 	
 	private void generateNether(World world,Random random, int x, int z){
