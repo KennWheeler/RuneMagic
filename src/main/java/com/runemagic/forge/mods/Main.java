6675323722534e04ddfefba2e4fdb1bdca077c5b
Ken <kennwheeler@hotmail.com>
2016-09-25 15:21:48 -0400

    Texture to Rune Essence Block
    First attempt to add texture to the Rune Essence Block

--- src/main/java/com/runemagic/forge/mods/Main.java
+++ src/main/java/com/runemagic/forge/mods/Main.java
@@ -4,6 +4,8 @@ import com.runemagic.forge.mods.ModBlocks.RuneEssenceBlock;
 import com.runemagic.forge.mods.ModItems.RuneEssence;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -26,6 +28,10 @@ public class Main {
 		
 		runeEssence = new RuneEssence();
 		GameRegistry.registerItem(runeEssence, "runeEssence");
+		
+		Item runeEssenceBlock = GameRegistry.findItem("runemagic", "runeEssenceBlock");
+		ModelResourceLocation runeEssenceBlockModel = new ModelResourceLocation("runemagic:runeEssenceBlock", "inventory");
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(runeEssenceBlock, 0, runeEssenceBlockModel);
 		
 		GameRegistry.registerWorldGenerator(new WorldGen(), 0);
 	}
